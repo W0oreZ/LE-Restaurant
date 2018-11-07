3c7cfbb1d087cc653537d9daa79ef7080ce204e7
W0oreZ <elkhadiri.abderrazak@gmail.com>
2018-11-07 18:35:55 +0100

    Laratrust Setup & Configuration

--- config/app.php
+++ config/app.php
@@ -173,6 +173,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * third party packages
+         */
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -205,6 +210,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
